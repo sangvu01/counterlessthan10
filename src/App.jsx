3fbc4c8f1e540317a7f16fc11492199a80c56d82
sangvu01 <vuminhsang01@gmail.com>
2026-05-26 11:59:30 +0700

    1st version of sangvu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import { useEffect } from 'react';
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+
 
 export default function App(){
-
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState([]);
 
+  // useEffect(() => {
+  //   setHistory(prev => {
+  //     let newlist = [...prev, count];
+  //     return newlist.slice(-5);
+  //   })
+  // }, [count]);
   useEffect(() => {
   setHistory(prev => {
     const newHistory = [...prev, count];
@@ -18,81 +21,45 @@ export default function App(){
     return newHistory.slice(-5);
   });
 }, [count]);
-return (
-  <div className="counter">
 
-    <div style={{ color: "red" }}>
-      cefhebfne
+
+
+
+
+
+
+  return(
+    <>
+    <div className="page">
+    <div className="count">
+      {count}
     </div>
-
-    <div className="count">{count}</div>
-
-    <div className="buttons">
-      <button
-        className="add"
-        onClick={() => count < 5 && setCount(count + 1)}
-      >
-        +
-      </button>
-
-      <button
-        className="sub"
-        onClick={() => count > -5 && setCount(count - 1)}
-      >
-        -
-      </button>
+    <div style={{
+      gap: "20px",
+      display: "flex"
+    }}>
+    <button onClick={() => (count < 10) && setCount(count + 1)}> + </button>
+    <button onClick={() => (count > -10) && setCount(count - 1)}> - </button>
     </div>
+    <div style={{
+      height: "24px",
+    }}>{(count === 10)? "MAX" :
+          (count === -10)? "MIN": " "}</div>
 
-    <div style={{height: "24px"}}>
-      {count === 5? "MAX"
-      : count === -5? "MIN"
-    : ""}
+    <div style={{
+      width: "100px",
+      borderBottom: "10px solid black",
+      borderTop: "10px solid red",
+      borderLeft: "10px solid blue",
+      borderRight: "10px solid purple"
+    }}>
+      HISTORY: </div>
+    <ul className="history">
+      {history.map((item,index) => (
+        <li key={index}> {item}</li>
+      ))}
+    </ul>
     </div>
-
-
-        <div>
-  <h3>History (5 gần nhất):</h3>
-  <ul>
-    {history.map((item, index) => (
-      <li key={index}>{item}</li>
-    ))}
-  </ul>
-</div>
-  </div>
-);
-//   return(
-//     <>
-
-//       <div style={{color: 'red'}}>
-//         cefhebfne
-//       </div>
-
-//       {count}
-
-      
-//       {/* {
-//   (count > 10) ? (
-//     <div>MAXIMUM</div>
-//   ) : (count < -10) ? (
-//     <div>MINIMUM</div>
-//   ) : null
-// } */}
-
-//       <button className="add" onClick={() =>{ if(count < 5) {setCount(count + 1)}}}> + </button>
-//       <button className="sub" onClick={() =>{ if(count > -5) {setCount(count - 1)}}}> - </button>
-      
-//     {
-//         (count === 5)? (
-//           <div>
-//             MAXIMUM
-//           </div>
-//         )
-//         : (count === -5)? (
-//           <div>
-//             MINIMUM
-//           </div>
-//         ): null
-//       }
-//     </>
-//   )
+    </>
+  )
 }
