759474e9958054af9549446c1c3635d56b3a79a5
sangvu01 <vuminhsang01@gmail.com>
2026-05-26 15:42:54 +0700

    level 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 
-
-export default function App(){
+export default function App() {
+  const final = 3;
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState([]);
 
@@ -14,52 +14,101 @@ export default function App(){
   //   })
   // }, [count]);
   useEffect(() => {
-  setHistory(prev => {
-    const newHistory = [...prev, count];
+    setHistory((prev) => {
+      const newHistory = [...prev, count];
 
-    // chỉ giữ 5 phần tử gần nhất
-    return newHistory.slice(-5);
-  });
-}, [count]);
-  return(
-    <>
-    <div className="All">
-    <div className="counttext">
-      COUNT: {count}
-    </div>
-    <div className="AllButtons">
-    <button onClick={() => (count < 10) && setCount(count + 1)} style={{
-      backgroundColor: "#26f901",
-      color: "red",
-    }}>
-       + 
-    </button>
-    <button onClick={() => (count > -10) && setCount(count - 1)} style={{
-      backgroundColor: "red",
-      color: "#26f901"
-    }}> - </button>
-    </div>
-    <div className="MinMax">{(count === 10)? "MAX" :
-          (count === -10)? "MIN": " "}</div>
+      // chỉ giữ 5 phần tử gần nhất
+      return newHistory.slice(-5);
+    });
+  }, [count]);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        backgroundColor: "red",
+        minHeight: "100vh",
+        padding: 0,
+      }}
+    >
+      <div style={{ display: "flex", flex: 1, backgroundColor: "red" }}>
+        <div
+          style={{
+            backgroundColor: "blue",
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+          }}
+        >
+          <div className="All">
+            <div className="Left">
+              <div className="Counter">COUNT: {count}</div>
+              <div className="buttons">
+                {/* <button onClick={() => count < final && setCount(prev => prev + 1)}> + </button>
+            <button onClick={() => count > -final && setCount(prev => prev - 1)}> - </button> */}
+                <button
+                  onClick={() => setCount((prev) => Math.min(prev + 1, final))}
+                  style={{
+                    backgroundColor: "#38fb03",
+                  }}
+                >
+                  {" "}
+                  +{" "}
+                </button>
+                <button
+                  onClick={() => setCount((prev) => Math.max(prev - 1, -final))}
+                  style={{
+                    backgroundColor: "#fb0303",
+                  }}
+                >
+                  {" "}
+                  -{" "}
+                </button>
+              </div>
+              <div className="alert">
+                {count === final ? "MAX" : count === -final ? "MIN" : ""}
+              </div>
+            </div>
 
-    <div className="history">
-      HISTORY: </div>
-    <ul>
-      {history.map((item,index) => (
-        <li key={index}> {item}</li>
-      ))}
-    </ul>
-    </div>
+            <div className="right">
+              <div className="historyTitle">HISTORY:</div>
+              <ul>
+                {history.map((value, key) => (
+                  <li key={key}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div
+            className="AB"
+            style={{
+              display: "flex",
+              flex: 1,justifyContent: "center",
+                alignItems: "center"
+              
+            }}
+          >
+            <div style={{
+              backgroundColor:
+                count === 3
+                  ? "#38fb03"
+                  : count === 2
+                    ? "yellow"
+                    : count === 1
+                      ? "red"
+                      : "black",
+                
+            }} className="A"></div>
+          </div>
+        </div>
 
-
-      {/* CSS PLAYGROUND */}
+        {/* CSS PLAYGROUND
       <div className="css-playground">
         <div className="box" onClick={() => {console.log("A")}}>A</div>
         <div className="box">B</div>
         <div className="box">C</div>
+      </div> */}
       </div>
-
-
-    </>
-  )
+    </div>
+  );
 }
