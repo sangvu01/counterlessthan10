a4333cdcf3246ee9f8b58ead430e350b1dd98d99
sangvu01 <vuminhsang01@gmail.com>
2026-05-27 11:21:44 +0700

    Card css

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,46 @@
-import { useState } from "react";
-import { useEffect } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
+import img from './assets/react.svg'
 import "./App.css";
 
 export default function App() {
-  const final = 3;
-  const [count, setCount] = useState(0);
-  const [history, setHistory] = useState([]);
 
-  // useEffect(() => {
-  //   setHistory(prev => {
-  //     let newlist = [...prev, count];
-  //     return newlist.slice(-5);
-  //   })
-  // }, [count]);
-  useEffect(() => {
-    setHistory((prev) => {
-      const newHistory = [...prev, count];
+  // const[message, setMessage] = useState("");
 
-      // chỉ giữ 5 phần tử gần nhất
-      return newHistory.slice(-5);
-    });
-  }, [count]);
+
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flex: 1,
-        backgroundColor: "red",
-        minHeight: "100vh",
-        padding: 0,
-      }}
-    >
-      <div style={{ display: "flex", flex: 1, backgroundColor: "red" }}>
-        <div
-          style={{
-            backgroundColor: "blue",
-            display: "flex",
-            flex: 1,
-            flexDirection: "column",
-          }}
-        >
-          <div className="All">
-            <div className="Left">
-              <div className="Counter">COUNT: {count}</div>
-              <div className="buttons">
-                {/* <button onClick={() => count < final && setCount(prev => prev + 1)}> + </button>
-            <button onClick={() => count > -final && setCount(prev => prev - 1)}> - </button> */}
-                <button
-                  onClick={() => setCount((prev) => Math.min(prev + 1, final))}
-                  style={{
-                    backgroundColor: "#38fb03",
-                  }}
-                >
-                  {" "}
-                  +{" "}
-                </button>
-                <button
-                  onClick={() => setCount((prev) => Math.max(prev - 1, -final))}
-                  style={{
-                    backgroundColor: "#fb0303",
-                  }}
-                >
-                  {" "}
-                  -{" "}
-                </button>
-              </div>
-              <div className="alert">
-                {count === final ? "MAX" : count === -final ? "MIN" : ""}
-              </div>
-            </div>
-
-            <div className="right">
-              <div className="historyTitle">HISTORY:</div>
-              <ul>
-                {history.map((value, key) => (
-                  <li key={key}>{value}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div
-            className="AB"
-            style={{
-              display: "flex",
-              flex: 1,justifyContent: "center",
-                alignItems: "center"
-              
-            }}
-          >
-            <div style={{
-              backgroundColor:
-                count === 3
-                  ? "#38fb03"
-                  : count === 2
-                    ? "yellow"
-                    : count === 1
-                      ? "red"
-                      : "black",
-                
-            }} className="A"></div>
-          </div>
-        </div>
-
-        {/* CSS PLAYGROUND
+    <div style={{
+      display: "flex",
+      flex: 1,
+      backgroundColor: "black",
+      minHeight: "100vh",
+      padding: 0,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+        {/* CSS PLAYGROUND */}
       <div className="css-playground">
-        <div className="box" onClick={() => {console.log("A")}}>A</div>
-        <div className="box">B</div>
-        <div className="box">C</div>
-      </div> */}
+        {/* <div className="card">
+          <img src={img} className='img'/>
+          <div className='right'><div className='name'>Nguyen Van A</div>
+          <div className='remain'>
+          <div className='job'> FE Dev</div>
+          <a href='https://chatgpt.com/' className='GPTlink'>GPT</a>
+          </div></div>
+        </div> */}
+        <div className="card">
+  <img src={img} className="img" />
+
+  <div className="name">Nguyen Van A</div>
+
+  <div className="bottom">
+    <div className="job">FE Dev</div>
+    <a className="GPTlink">GPT</a>
+  </div>
+</div>
       </div>
+
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,103 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+// body {
+//   margin: 0;
+//   font-family: Arial, sans-serif;
+// }
+
+// .card {
+//   background-color: white;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   /* justify-content: center; */
+//   gap: 12px;
+
+//   border-radius: 16px;
+//   width: 350px;
+//   height: 250px;
+
+//   box-shadow: 0 4px 12px rgba(0,0,0,0.2);
+
+//   transition: 0.3s;
+// }
+
+// .card:hover {
+//   transform: translateY(-5px);
+// }
+
+// .img {
+//   width: 80px;
+//   height: 80px;
+//   border-radius: 50%;
+// }
+
+// .name {
+//   font-size: 24px;
+//   font-weight: bold;
+//   text-align: right;
+// }
+
+// .job {
+//   color: gray;
+//   font-size: 18px;
+// }
+
+// .GPTlink {
+//   text-decoration: none;
+//   /* background-color: #007bff; */
+//   color: #373e03;
+//   font-weight: 800;
+//   /* padding: 10px 20px;
+//   border-radius: 8px; */
+
+//   transition: 0.3s;
+// }
+
+// .GPTlink:hover {
+//   background-color: #0056b3;
+// }
+
+
+// .remain{
+//   display: flex;
+//   gap: 30px
+// }
+
+// /* body {
+//   margin: 0;
+// }
+
+
+
+
+
+// .card{
+//   background-color: white;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 20px;
+//   border-radius: 10px;
+//   width: 350px;
+//   height: 200px;
+// }
+
+// .img{
+
+// }
+
+// .name{
+
+// }
+
+// .job{
+
+// }
+
+// .GPTlink{
+
+// }
+//  */
